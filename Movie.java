eb05856278089226addb81ea6782ba1d173ad765
arthursl12 <arthursoutolima@gmail.com>
2020-10-14 14:52:04 -0300

    Commit12

--- Movie.java
+++ Movie.java
@@ -32,29 +32,14 @@ public class Movie {
         }
      }
   
+
     public String getTitle (){
         return _title;
     }
 
-	public double getCharge(int daysRented) {
-	      double amount = 0;
-	      switch (getPriceCode()) {
-	         case Movie.REGULAR:
-	            amount += 2;
-	            if (daysRented > 2)
-	               amount += (daysRented - 2) * 1.5;
-	            break;
-	         case Movie.NEW_RELEASE:
-	            amount += daysRented * 3;
-	            break;
-	         case Movie.CHILDRENS:
-	            amount += 1.5;
-	            if (daysRented > 3)
-	               amount += (daysRented - 3) * 1.5;
-	             break;
-	      }
-	      return amount;
-       }
+    public double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
     
     public int getFrequentRenterPoints(int daysRented) {
         // add bonus for a two day new release rental
