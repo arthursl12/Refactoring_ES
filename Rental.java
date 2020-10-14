76a7c53c1a988458c71d2572ff8cf43b9db33f29
arthursl12 <arthursoutolima@gmail.com>
2020-10-14 14:44:45 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -1,6 +1,6 @@
 public class Rental {
 
-    private Movie _movie;
+    Movie _movie;
     private int _daysRented;
  
     public Rental(Movie movie, int daysRented) {
@@ -16,25 +16,9 @@ public class Rental {
        return _movie;
     }
 
-    public double getCharge() {
-	      double amount = 0;
-	      switch (getMovie().getPriceCode()) {
-	         case Movie.REGULAR:
-	            amount += 2;
-	            if (getDaysRented() > 2)
-	               amount += (getDaysRented() - 2) * 1.5;
-	            break;
-	         case Movie.NEW_RELEASE:
-	            amount += getDaysRented() * 3;
-	            break;
-	         case Movie.CHILDRENS:
-	            amount += 1.5;
-	            if (getDaysRented() > 3)
-	               amount += (getDaysRented() - 3) * 1.5;
-	             break;
-	      }
-	      return amount;
-	   }
+    public double getCharge(){
+      return _movie.getCharge(_daysRented);
+   }
 
     public int getFrequentRenterPoints() {
 	      // add bonus for a two day new release rental
